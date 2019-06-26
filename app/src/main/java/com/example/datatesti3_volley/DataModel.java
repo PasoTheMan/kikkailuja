414ff61cbbc15f53998c258c661795f523f47453
PasoTheMan <pasi.karjalainen@hotmail.com>
2019-06-26 17:00:25 +0300

    Using Volley to get data from server.

---
+++ app/src/main/java/com/example/datatesti3_volley/DataModel.java
@@ -0,0 +1,22 @@
+package com.example.datatesti3_volley;
+
+public class DataModel {
+
+    private String ID, Pressure;
+
+    public String getPressure() {
+        return Pressure;
+    }
+
+    public void setPressure(String pressure) {
+        this.Pressure = pressure;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String id) {
+        this.ID = id;
+    }
+}
---
+++ app/src/main/java/com/example/datatesti3_volley/MainActivity.java
@@ -0,0 +1,144 @@
+package com.example.datatesti3_volley;
+
+//https://demonuts.com/android-listview-using-volley/
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String URLstring = "http://91.158.253.42/fetch_test.php";
+    private static ProgressDialog mProgressDialog;
+    private ListView listView;
+    ArrayList<DataModel> dataModelArrayList;
+    private ListAdapter listAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.lv);
+
+        retrieveJSON();
+
+    }
+
+    private void retrieveJSON() {
+
+        showSimpleProgressDialog(this, "Loading...","Fetching Json",false);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URLstring,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Log.d("strrrrr", ">>" + response);
+
+                        try {
+
+                            JSONObject obj = new JSONObject(response);
+                            if(obj.optString("status").equals("true")){
+
+                                dataModelArrayList = new ArrayList<>();
+                                JSONArray dataArray  = obj.getJSONArray("data"); // ????????????????????????????????
+
+                                for (int i = 0; i < dataArray.length(); i++) {
+
+                                    DataModel playerModel = new DataModel();
+                                    JSONObject dataobj = dataArray.getJSONObject(i);
+
+                                    playerModel.setID(dataobj.getString("id"));
+                                    playerModel.setPressure(dataobj.getString("pressure"));
+
+                                    dataModelArrayList.add(playerModel);
+
+                                }
+
+                                setupListview();
+
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //displaying the error in toast if occurrs
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // request queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        requestQueue.add(stringRequest);
+
+
+    }
+
+    private void setupListview(){
+        removeSimpleProgressDialog();  //will remove progress dialog
+        listAdapter = new ListAdapter(this, dataModelArrayList);
+        listView.setAdapter(listAdapter);
+    }
+
+    public static void removeSimpleProgressDialog() {
+        try {
+            if (mProgressDialog != null) {
+                if (mProgressDialog.isShowing()) {
+                    mProgressDialog.dismiss();
+                    mProgressDialog = null;
+                }
+            }
+        } catch (IllegalArgumentException ie) {
+            ie.printStackTrace();
+
+        } catch (RuntimeException re) {
+            re.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void showSimpleProgressDialog(Context context, String title,
+                                                String msg, boolean isCancelable) {
+        try {
+            if (mProgressDialog == null) {
+                mProgressDialog = ProgressDialog.show(context, title, msg);
+                mProgressDialog.setCancelable(isCancelable);
+            }
+
+            if (!mProgressDialog.isShowing()) {
+                mProgressDialog.show();
+            }
+
+        } catch (IllegalArgumentException ie) {
+            ie.printStackTrace();
+        } catch (RuntimeException re) {
+            re.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
